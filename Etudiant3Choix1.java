d2e0cc3c142449ec225e2c825284bd36455d1a2b
ArthurQuarteroni <84666372+beaugossedu12@users.noreply.github.com>
2021-12-05 20:47:14 +0100

    Add files via upload

---
+++ Etudiant3Choix1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.insa.quarteroni.Interface;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+/**
+ *
+ * @author arthurquarteroni
+ */
+public class Etudiant3Choix1 extends VerticalLayout {
+
+    private VuePrincipale main;
+ 
+    
+
+    public Etudiant3Choix1(VuePrincipale main) {
+
+        
+   
+    }
+}
