9a58080600a24d9911f5486eb17b3d0e9b4ee700
ArthurQuarteroni <84666372+beaugossedu12@users.noreply.github.com>
2022-01-03 20:33:12 +0100

    Update Etudiant4Choix2.java

--- Etudiant4Choix2.java
+++ Etudiant4Choix2.java
@@ -1,13 +1,15 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package fr.insa.quarteroni.Interface;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.combobox.ComboBox;
-import com.vaadin.flow.component.combobox.ComboBox.ItemFilter;
+import com.vaadin.flow.component.html.H3;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.component.textfield.TextFieldVariant;
 
@@ -18,61 +20,131 @@ import com.vaadin.flow.component.textfield.TextFieldVariant;
 public class Etudiant4Choix2 extends VerticalLayout {
 
     private VuePrincipale main;
-    private ComboBox Choix1;
-    private ComboBox Choix2;
-    private ComboBox Choix3;
-    private TextField titre;
+    private H3 titre;
+    private HorizontalLayout choix1;
+    private TextField titrechoix1;
+    private Button module1;
+    private Button module2;
+    private Button module3;
     private Button valider;
+    private final TextField titrechoix2;
+    private final HorizontalLayout choix2;
+    private final Button module12;
+    private final Button module22;
+    private final Button module32;
+    private final TextField titrechoix3;
+    private final HorizontalLayout choix3;
+    private final Button module13;
+    private final Button module23;
+    private final Button module33;
+    private final Button retour;
 
     public Etudiant4Choix2(VuePrincipale main) {
-        
-        this.main = main;
-        
 
-        this.titre = new TextField();
-        this.titre.setValue("Groupe de Modules 2");
-        this.titre.setReadOnly(true);
-        this.titre.setSizeFull();
-        this.titre.addThemeVariants(TextFieldVariant.LUMO_ALIGN_CENTER);
+        this.main = main;
+
+        this.titre = new H3("GROUPE DE MODULES 2");
+        titre.setSizeFull();
         this.add(this.titre);
 
-        this.Choix1 = new ComboBox();
-        this.add(this.Choix1);
-        this.Choix1.setLabel("Choix 1");
-        this.Choix1.setWidth("20%");
-        this.Choix1.getStyle().set("--vaadin-combo-box-overlay-width", "350px");
-        this.Choix1.setPlaceholder("Choisissez un module");
-        //Collection Modules = collection des modules dans la bdd;
-        //this.Choix1.setItems(Modules);
+        this.titrechoix1 = new TextField();
+        this.titrechoix1.setValue("Choix 1");
+        this.titrechoix1.setReadOnly(true);
+        this.titrechoix1.setSizeFull();
+        this.titrechoix1.addThemeVariants(TextFieldVariant.LUMO_ALIGN_CENTER);
+        this.add(this.titrechoix1);
 
-        this.Choix2 = new ComboBox();
-        this.add(this.Choix2);
-        this.Choix2.setLabel("Choix 2");
-        this.Choix2.setWidth("20%");
-        this.Choix2.getStyle().set("--vaadin-combo-box-overlay-width", "350px");
-        this.Choix2.setPlaceholder("Choisissez un module");
-        //Collection Modules = collection des modules dans la bdd;
-        //this.Choix1.setItems(Modules);
+        this.choix1 = new HorizontalLayout();
+        this.choix1.setWidthFull();
+        this.choix1.setHeight("25%");
+        this.choix1.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+        this.add(this.choix1);
+        this.choix1.setMargin(true);
 
-        this.Choix3 = new ComboBox();
-        this.add(this.Choix3);
-        this.Choix3.setLabel("Choix 3");
-        this.Choix3.setWidth("20%");
-        this.Choix3.getStyle().set("--vaadin-combo-box-overlay-width", "350px");
-        this.Choix3.setPlaceholder("Choisissez un module");
-        //Collection Modules = collection des modules dans la bdd;
-        //this.Choix1.setItems(Modules);
+        this.module1 = new Button("Module 1");
+        this.choix1.add(this.module1);
+        module1.setWidth("30%");
+        module1.setDisableOnClick(true);
+        this.module2 = new Button("Module 2");
+        this.choix1.add(this.module2);
+        module2.setWidth("30%");
+        module2.setDisableOnClick(true);
+        this.module3 = new Button("Module 3");
+        this.choix1.add(this.module3);
+        module3.setWidth("30%");
+        module3.setDisableOnClick(true);
+
+        this.titrechoix2 = new TextField();
+        this.titrechoix2.setValue("Choix 2");
+        this.titrechoix2.setReadOnly(true);
+        this.titrechoix2.setSizeFull();
+        this.titrechoix2.addThemeVariants(TextFieldVariant.LUMO_ALIGN_CENTER);
+        this.add(this.titrechoix2);
+
+        this.choix2 = new HorizontalLayout();
+        this.choix2.setWidthFull();
+        this.choix2.setHeight("25%");
+        this.choix2.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+        this.add(this.choix2);
+        this.choix2.setMargin(true);
+
+        this.module12 = new Button("Module 1");
+        this.choix2.add(this.module12);
+        module12.setWidth("30%");
+        module12.setDisableOnClick(true);
+        this.module22 = new Button("Module 2");
+        this.choix2.add(this.module22);
+        module22.setDisableOnClick(true);
+        module22.setWidth("30%");
+        this.module32 = new Button("Module 3");
+        this.choix2.add(this.module32);
+        module32.setWidth("30%");
+        module32.setDisableOnClick(true);
+
+        this.titrechoix3 = new TextField();
+        this.titrechoix3.setValue("Choix 3");
+        this.titrechoix3.setReadOnly(true);
+        this.titrechoix3.setSizeFull();
+        this.titrechoix3.addThemeVariants(TextFieldVariant.LUMO_ALIGN_CENTER);
+        this.add(this.titrechoix3);
+
+        this.choix3 = new HorizontalLayout();
+        this.choix3.setWidthFull();
+        this.choix3.setHeight("25%");
+        this.choix3.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+        this.add(this.choix3);
+        this.choix3.setMargin(true);
+
+        this.module13 = new Button("Module 1");
+        this.choix3.add(this.module13);
+        module13.setWidth("30%");
+        module13.setDisableOnClick(true);
+        this.module23 = new Button("Module 2");
+        this.choix3.add(this.module23);
+        module23.setWidth("30%");
+        module23.setDisableOnClick(true);
+        this.module33 = new Button("Module 3");
+        this.choix3.add(this.module33);
+        module33.setWidth("30%");
+        module33.setDisableOnClick(true);
 
         this.valider = new Button("Valider");
         this.add(this.valider);
         valider.setWidthFull();
 
+        this.retour = new Button("Retour");
+        this.add(this.retour);
+        this.retour.addClickListener((e) -> {
+            this.main.changeContenu(new Etudiant3Choix1(this.main));
+            this.main.reculerBarre(main);
+        });
+
         this.valider.addClickListener((e) -> {
 
-            this.main.changeContenu(new EtudiantFin(this.main));
+            this.main.changeContenu(new Etudiant5Choix3(this.main));
             this.main.avancerBarre(this.main);
-            this.main.avancerBarre(this.main);
-            this.main.avancerBarre(this.main);
-        } );
-        }
+
+        });
     }
+
+}
